f80318e14e4803dad4ca56db32cb9913f7a76d00
Chai Keshab <keshab3886@gmail.com>
2023-09-20 17:36:30 +0545

    end-point to delete notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -49,30 +49,55 @@ router.post('/addnote', fetchUser, [
 
 // ROUTE 3: Update an existing Note using: PUT "/api/notes/updatenote/:id". Login required. Logged in means having auth-token which is send in header
 router.put('/updatenote/:id', fetchUser, [
-    // body('title', 'Enter a valid title').isLength({ min: 1 }),
-    // body('description', 'Enter a valid email').isLength({ min: 1 })
 ], async (req, res) => {
     const { title, description, tags } = req.body
 
-    //Create a newNote object
-    const newNote = {};
-    if (title) { newNote.title = title };
-    if (description) { newNote.description = description };
-    if (tags) { newNote.tags = tags };
+    try {
+        //Create a newNote object
+        const newNote = {};
+        if (title) { newNote.title = title };
+        if (description) { newNote.description = description };
+        if (tags) { newNote.tags = tags };
 
-    //Find the note to be updated and update it
-    let note = await Note.findById(req.params.id);
-    if (!note) { return res.status(404).send("Not Found") }
+        //Find the note to be updated and update it
+        let note = await Note.findById(req.params.id);
+        if (!note) { return res.status(404).send("Not Found") }
 
-    if (note.user.toString() !== req.user.id) {
-        return res.status(401).json("Not Allowed")
+        //Allow updation only if user owns this Note
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).json("Not Allowed")
+        }
+
+        note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+        res.json({ note });
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).send("Internal Server Error")
     }
-
-    note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
-    res.json({ note });
 
 })
 
-// ROUTE 3: Update an existing Note using: PUT "/api/notes/updatenote/:id". Login required. Logged in means having auth-token which is send in header
+// ROUTE 4: Delete an existing Note using: DELETE "/api/notes/deletenote/:id". Login required.
+router.delete('/deletenote/:id', fetchUser, [
+], async (req, res) => {
 
+    try {
+        //Find the note to be deleted and delelte it
+        let note = await Note.findById(req.params.id);
+        if (!note) { return res.status(404).send("Not Found") }
+
+        //Allow deletetion only if user owns this Note
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).json("Not Allowed")
+        }
+
+        note = await Note.findByIdAndDelete(req.params.id)
+        res.json({ "Success": "Note has been deleted", note: note });
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).send("Internal Server Error")
+    }
+})
 module.exports = router
